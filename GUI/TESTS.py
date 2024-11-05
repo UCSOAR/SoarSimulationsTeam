35408d75975e732a39bd87c3478c75fa5b561163
zackgusikoski <98663093+zackgusikoski@users.noreply.github.com>
2024-11-04 21:54:39 -0700

    Update TESTS.py
    Motor, rocket and help pages defined as well as buttons to move between them, a start page and the tkinter logo in the top left was replaced with the MAPLEAF logo

--- GUI/TESTS.py
+++ GUI/TESTS.py
@@ -1,26 +1,51 @@
-from tkinter import *
 import matplotlib.pyplot as plt
 import functions
-import pages
+from tkinter import *
 
-#initialize GUI window
 window = Tk()
+window.title("MAPLEAF")
 
-#title the window
-window.title('MAPLEAF')
+window.iconphoto(False, PhotoImage(file='img/Logo.png'))
+
+menu = Frame(window)
+menu.pack(fill=Y)
+
+page = Frame(master=window, height=500)
+page.pack(fill=X)
+
 
 #take and display SOAR.png photo from computer
 photo_directory = functions.img_resource_path("img/Logo.png")
 logo = PhotoImage(file=photo_directory)
-logo_label = Label(window, image=logo)
+logo_label = Label(page, image=logo)
+#welcome_message = Label(page, text="Welcome to MAPLEAF").grid(column=1, row=2)
 logo_label.grid(column=1, row=1)
 
-#window size
-window.geometry('800x600')
+page.pack(fill=X)
 
-#button to run MAPLEAF
-Button (window, text='Run Simulation', width=12, command=functions.run, activebackground='red', foreground='red',).grid(column=2, row=13)
-Button (window, text='Motor', width=12, command=pages.page1(window), activebackground='red', foreground='red',).grid(column=2, row=15)
+# clear screen function
+def clear(object):
+    slaves = object.grid_slaves()
+    for x in slaves:
+        x.destroy()
+ 
+# different items on different menus
+def motor_page():
+    clear(page)
+    Label(page, text="BRRRRR").grid(row=0, column=0)
+ 
+def rocket_page():
+    clear(page)
+    Checkbutton(page, text="Does rocket exist?").grid(row=0, column=0)
+    Label(page, text="Rocket stuff...").grid(row=1, column=0)
 
-#run window loop
-window.mainloop()
+def help_page():
+    clear(page)
+    Label(page, text="Wish I could help... StackOverFlow??").grid(row=1, column=0)
+
+# persistent menu buttons
+motor_button = Button(menu, text="Motor", command=motor_page).grid(row=0, column=0)
+rocket_button2 = Button(menu, text="Rocket", command=rocket_page).grid(row=0, column=1)
+help_button = Button (menu, text="Help", command=help_page).grid(row=0, column=2)
+ 
+mainloop()
